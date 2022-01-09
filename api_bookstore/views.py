2241015b87a1abd0d74b88c15bfaed54d0c1d7c6
Adolfo <93218545+AdolfR6@users.noreply.github.com>
2022-01-09 14:39:05 +0100

    Subida Reto 12
    Subida del Reto 12

---
+++ api_bookstore/views.py
@@ -0,0 +1,24 @@
+from rest_framework import viewsets
+from .serializers import AuthorSerializers, BookSerializers
+from .models import Author, Book
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
+@api_view(["GET"])
+@csrf_exempt
+@permission_classes([IsAuthenticated])
+
+def welcome(request):
+    content = {"message": "Welcome to the BookStore!"}
+    return JsonResponse(content)
+
+# Create your views here.
+class AuthorViewSet(viewsets.ModelViewSet):
+    queryset = Author.objects.all().order_by('name')
+    serializer_class = AuthorSerializers
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all().order_by('title')
+    serializer_class = BookSerializers
